b5def23378ee280399ab88cb84de581b569ef109
Brunolbahiense <brunoluisbahiense@gmail.com>
2021-10-15 13:11:02 -0300

    meta tags added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,37 @@ function App({ Component, pageProps }: AppProps) {
           name="description"
           content="Using the pomodoro method to improve your productivity 🚀"
         />
+
+        <title>Tomatracker</title>
+        <meta name="title" content="Tomatracker" />
+        <meta
+          name="description"
+          content="Improve your productivity using The Pomodoro Technique"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Tomatracker" />
+        <meta
+          property="og:description"
+          content="Improve your productivity using The Pomodoro Technique"
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Tomatracker" />
+        <meta
+          property="twitter:description"
+          content="Improve your productivity using The Pomodoro Technique"
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
