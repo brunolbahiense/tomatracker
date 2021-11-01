5dc8d4f840dbd1797da4da73d6c053706f6019c8
Brunolbahiense <brunoluisbahiense@gmail.com>
2021-11-01 14:07:48 -0300

    error fixed

--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -2,7 +2,7 @@ import * as S from './styles'
 
 export interface WrapperProps {
   background: 'blue' | 'red'
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 const Wrapper = ({ background = 'red', ...props }: WrapperProps) => {
   return <S.Wrapper {...props} background={background} />
